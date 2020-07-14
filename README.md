24eef4fe61822462ab55d8580ebcbcb07dc0b92b
fitsum <fitsumbelay@gmail.com>
2020-07-14 00:05:28 -0400

    :clock12: UNO

---
+++ README.md
@@ -0,0 +1,12 @@
+# CONSOLE-OVERLOG
+
+## INFO
+- node module that console.logs stuff on the same line
+
+## HOWTO
+- `npm i https://github.com/fitsum/console-overlog`
+
+## TODO
+- make enhancements
+- fix bugs
+
---
+++ index.js
@@ -0,0 +1,15 @@
+
+// write a line to console
+const overlog = (content) => {
+    process.stdout.write(content);
+    cr();								
+}
+
+// carriage return, same line 
+// why is this seperate
+const cr = () => {
+	   process.stdout.cursorTo(0);
+}
+
+module.exports = { overlog };
+
