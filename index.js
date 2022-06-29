20e382bdfc15c7bec1fb6ba1afe791652e5a3396
fitsum <fitsumbelay@gmail.com>
2022-06-29 19:56:40 -0400

    remove seperate fn for carraige return

--- index.js
+++ index.js
@@ -1,13 +1,8 @@
 // write a line to console
 const overlog = (content) => {
   process.stdout.write(content);
-  cr();
-};
-
-// carriage return, same line
-// why is this seperate
-const cr = () => {
   process.stdout.cursorTo(0);
+  // cr();
 };
 
 module.exports = overlog;
