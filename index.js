68ae47fc1f00a9462400bab8dfe31ba0d0aac6f2
fitsum <fitsumbelay@gmail.com>
2022-06-29 23:27:34 -0400

    decruft

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 // write a line to console
 const overlog = input => {
-  if (input === '\r\n') { process.stdout.write(input); return; }
   process.stdout.write(input);
-  process.stdout.cursorTo(0);
+  if (input !== '\r\n') process.stdout.cursorTo(0);
 };
 
 module.exports = overlog;
