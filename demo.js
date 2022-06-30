f9fbb0388fc25d652adc51308efb6317d78cfe9a
fitsum <fitsumbelay@gmail.com>
2022-06-29 22:58:00 -0400

    use numbers for keys

--- demo.js
+++ demo.js
@@ -1,15 +1,15 @@
 const c = require('./index');
 const lines = new Map();
-lines.set('1', 'there is a house in New Orleans');
-lines.set('2', 'they call the Rising Sun');
-lines.set('3', 'and it has been the ruin of many a poor boy');
-lines.set('4', 'in god I know I have won');
+lines.set(1, 'there is a house in New Orleans');
+lines.set(2, 'they call the Rising Sun');
+lines.set(3, 'and it has been the ruin of many a poor boy');
+lines.set(4, 'in god I know I have won');
 
 let counter = 0;
 const write = (lines, interval) => {
   if (counter >= lines.size) { if (interval) clearInterval(interval); return; }
   counter++;
-  c(lines.get(counter.toString()));
+  c(lines.get(counter));
 };
 
 write(lines);
