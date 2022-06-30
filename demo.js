7fce26124c83adad40d3ef75860e0bf723e8a6c5
fitsum <fitsumbelay@gmail.com>
2022-06-29 23:28:20 -0400

    use array instead of Map + update demo code

--- demo.js
+++ demo.js
@@ -1,15 +1,15 @@
 const c = require('./index');
-const lines = new Map();
-lines.set(1, 'there is a house in New Orleans');
-lines.set(2, 'they call the Rising Sun');
-lines.set(3, 'and it has been the ruin of many a poor boy');
-lines.set(4, 'in god I know I have won');
+const lines = [];
+lines.push('there is a house in New Orleans');
+lines.push('they call the Rising Sun');
+lines.push('and it has been the ruin of many a poor boy');
+lines.push('in god I know I have won');
 
 let counter = 0;
 const write = (lines, interval) => {
-  if (counter >= lines.size) { if (interval) { clearInterval(interval); } c('\r\n'); return; }
+  if (counter >= lines.length) { if (interval) { clearInterval(interval); } c('\r\n'); return; }
+  c(lines[counter]);
   counter++;
-  c(lines.get(counter));
 };
 
 write(lines);
