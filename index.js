c7eb1c8f83c8dcad619cdf0c05b5ab0f4523d732
fitsum <fitsumbelay@gmail.com>
2022-07-01 00:06:55 -0400

    add boolean arg to overlog() to completely overwrite last line + update demo

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
+// memo
+let lastInput;
+
 // write a line to console
-const overlog = input => {
+const overlog = (input, cleanInput) => {
+  if (cleanInput && typeof lastInput !== 'undefined') {
+    const diff = lastInput.length - input.length;
+    if (diff > 0) { input += Array(diff).fill(' ').join(''); }
+  }
   process.stdout.write(input);
+  lastInput = input;
   if (input !== '\r\n') process.stdout.cursorTo(0);
 };
 
