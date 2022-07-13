5b34189ff849c76b880b39cdd5cf44812aa1d4c7
fitsum <fitsumbelay@gmail.com>
2022-07-12 23:17:13 -0400

    use ES import syntax + re-lint

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 // memo
-let lastInput;
+let lastInput
 
 // write a line to console
-const overlog = (input, cleanInput) => {
-  if (cleanInput && typeof lastInput !== 'undefined') {
-    const diff = lastInput.length - input.length;
-    if (diff > 0) { input += Array(diff).fill(' ').join(''); }
+const overlog = ( input, cleanInput ) => {
+  if ( cleanInput && typeof lastInput !== 'undefined' ) {
+    const diff = lastInput.length - input.length
+    if ( diff > 0 ) { input += Array( diff ).fill( ' ' ).join( '' ) }
   }
-  process.stdout.write(input);
-  lastInput = input;
-  if (input !== '\r\n') process.stdout.cursorTo(0);
-};
+  process.stdout.write( input )
+  lastInput = input
+  if ( input !== '\r\n' ) process.stdout.cursorTo( 0 )
+}
 
-module.exports = overlog;
+// module.exports = overlog;
+export { overlog }
